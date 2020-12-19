15326517ea718992616a1625f6d906fff4e8b0b4
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2020-12-19 17:36:14 +0000

    More UI work

---
+++ Grid System/Assets/Scripts/MapPainter/Controllers/MapPainterController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapPainterController : MonoBehaviour
+{
+    public GameObject highlight;
+    public StringVariable CoordText;
+    public BoolVariable UIInteraction;
+    public BoolVariable MouseOnScreen;
+
+    HexagonMapManager hexMap;
+
+    Dictionary<string, Tile> tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIEvents.current.OnTileClicked += TileSelected;
+
+        NewMap();
+        tiles = hexMap.GetTileAssets();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!UIInteraction.value && MouseOnScreen.value)
+        {
+            highlight.gameObject.SetActive(true);
+            highlight.transform.position = hexMap.GetSelectorPosition(Camera.main);
+            CheckMouse();
+        } else
+		{
+            highlight.gameObject.SetActive(false);
+		}
+    }
+
+    void CheckMouse()
+    {
+        Vector3Int mousePos = hexMap.GridPosFromMouse(Camera.main);
+        if (Input.GetMouseButton(0))
+        {
+            hexMap.SetTileAtPos(hexMap.GridPosFromMouse(Camera.main), "Tile1");
+        } 
+        else if (Input.GetMouseButton(1))
+		{
+            hexMap.SetTileAtPos(hexMap.GridPosFromMouse(Camera.main), null);
+        }
+        CoordText.value = $"{mousePos.x},{mousePos.y}";
+    }
+
+    void NewMap()
+	{
+        hexMap = new HexagonMapManager();
+        hexMap.CreateNewMap("HexBoard/Tiles", "NewMap");
+        hexMap.SetTileAtPos(new Vector3Int(0, 0, 0), "Tile1");
+    }
+
+    void TileSelected(int tileNum)
+	{
+	}
+
+	private void OnDestroy()
+	{
+        UIEvents.current.OnTileClicked -= TileSelected;
+    }
+}
---
+++ Grid System/Assets/Scripts/MapPainter/Controllers/UIEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour
+{
+    public static UIEvents current;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public event Action OnNewMapClicked;
+    public void NewMapClicked() => OnNewMapClicked?.Invoke();
+
+    public event Action<int> OnTileClicked;
+    public void TileClicked(int tileNum) => OnTileClicked?.Invoke(tileNum);
+}
