18e324a9ba2871e78c4f9d9587063d97c97ec646
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2021-01-01 15:59:42 +0000

    Added textmeshpro to UI and began improving resource loading

---
+++ Assets/Scripts/MapPainter/Controllers/Initialiser.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Initialiser : MonoBehaviour
+{
+	private void Awake()
+	{
+		if (!Directory.Exists($"{Application.dataPath}/TileSets"))
+		{
+			Directory.CreateDirectory($"{Application.dataPath}/TileSets");
+		}
+		if (!Directory.Exists($"{Application.dataPath}/Exports"))
+		{
+			Directory.CreateDirectory($"{Application.dataPath}/Exports");
+		}
+	}
+}
